f8c0a0900670cac512dc0fb88a2f5d310a37d287
BobrovnikIlya <ilysha.bobrovnik@gmail.com>
2024-02-07 14:16:06 +0300

    initial commit

---
+++ src/main/java/com/example/cpma/CpmaApplication.java
@@ -0,0 +1,12 @@
+package com.example.cpma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmaApplication.class, args);
+	}
+}
