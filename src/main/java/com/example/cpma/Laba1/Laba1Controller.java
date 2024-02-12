3effe15b753ce9f6c47e1d34fb738150f9b76139
BobrovnikIlya <ilysha.bobrovnik@gmail.com>
2024-02-12 13:22:24 +0300

    Generating key table and text output

---
+++ src/main/java/com/example/cpma/Laba1/Laba1Controller.java
@@ -0,0 +1,114 @@
+package com.example.cpma.Laba1;
+
+import com.example.cpma.Laba1.Task;
+import com.example.cpma.Laba1.VerticalPermutationCipher;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+import static com.example.cpma.Laba1.VerticalPermutationCipher.writeToFile;
+
+@RestController
+public class Laba1Controller {
+    Task task = new Task();
+    String inputFile = "src/main/resources/input.txt"; // Имя файла с вводной строкой
+    String encryptedFile = "src/main/resources/encrypted.txt"; // Файл для зашифрованной строки
+    String decryptedFile = "src/main/resources/decrypted.txt"; // Файл для расшифрованной строки
+    @GetMapping("/laba1")
+    public ModelAndView viewLaba1() {
+        System.out.println("Запуск страницы с первой лабой");
+        System.out.println("Чтение предложения из файла");
+        try {
+            task.setText(VerticalPermutationCipher.readFromFile(inputFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ModelAndView view = new ModelAndView("laba1");
+        view.addObject("textInput", task.getText());
+        view.addObject("numbers", task.getPermutation());
+        view.addObject("inputKey", task.getKey());
+        view.addObject("textOutput", task.getEncrypted());
+        System.out.println("Отображение страницы с первой лабой");
+        return view;
+    }
+
+    @PostMapping("/inputKey")
+    public ModelAndView inputKey(@RequestParam("inputKey") String textInput) {
+        System.out.println("Полученные данные: " + textInput);
+        int key = Integer.parseInt(textInput);
+
+        task.setKey(key);
+        task.setPermutation(VerticalPermutationCipher.Permutation(key));
+
+        return new ModelAndView("redirect:/laba1");
+    }
+
+    @PostMapping("/inputTable")
+    public ModelAndView inputTable() {
+        System.out.println("Заполнение таблицы");
+        return new ModelAndView("redirect:/laba1");
+    }
+
+    @PostMapping("/encryptedText")
+    public ModelAndView encryptedText() throws IOException {
+        System.out.println("Шифрование");
+
+        task.setEncrypted(VerticalPermutationCipher.encrypt(task.getText(), task.getPermutation()));
+        writeToFile(encryptedFile, task.getEncrypted());
+        System.out.println("Зашифрованная строка сохранена в файл " + encryptedFile);
+
+        return new ModelAndView("redirect:/laba1");
+    }
+
+    @PostMapping("/decryptedText")
+    public ModelAndView decryptedText() throws IOException {
+        System.out.println("Дешифрование");
+
+        task.setDecrypted(VerticalPermutationCipher.decrypt(task.getEncrypted(), task.getPermutation()));
+        writeToFile(decryptedFile, task.getDecrypted());
+        System.out.println("Расшифрованная строка сохранена в файл " + decryptedFile);
+
+        return new ModelAndView("redirect:/laba1");
+    }
+
+/*    @PostMapping("/redirect")
+    public ModelAndView redirect(@RequestParam String option, @ModelAttribute Task task, HttpSession session) {
+        System.out.println("Произведение расчетов...");
+        task.setListWrapper();
+        System.out.println("Передача данных с клиента на сервер");
+        if (option.equals("Simplex")) {
+            System.out.println("Запуск решения симплекс-методом");
+            CompleteSimplex completeSimplex = SimplexMethod.getCompleteTask(task);
+            System.out.println("Сохранение результатов...");
+            session.setAttribute("task",task);
+            session.setAttribute("simplex",completeSimplex);
+            System.out.println("Результаты сохранены. Переход к отображению результатов");
+            return new ModelAndView("redirect:/Simplex");
+        } else if (option.equals("Hook")) {
+            System.out.println("Запуск решения методом Хука-Дживса");
+            CompleteHook completeHook =  HookMethods.solveResult(task);
+            System.out.println("Сохранение результатов...");
+            session.setAttribute("task",task);
+            session.setAttribute("hook",completeHook);
+            System.out.println("Результаты сохранены. Переход к отображению результатов");
+            return new ModelAndView("redirect:/Hook");
+        }
+        System.out.println("Не выбран метод...");
+        return new ModelAndView("Home");
+    }*/
+    @GetMapping("/ForUser")
+    public ModelAndView userPage() {
+        System.out.println("Отображение руководства пользователю");
+        ModelAndView user = new ModelAndView("ForUser");
+        return user;
+    }
+    @GetMapping("/About")
+    public ModelAndView aboutPage() {
+        System.out.println("Отображение информации об авторе");
+        ModelAndView about = new ModelAndView("About");
+        return about;
+    }
+}
---
+++ src/main/java/com/example/cpma/Laba1/VerticalPermutationCipher.java
@@ -0,0 +1,107 @@
+package com.example.cpma.Laba1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class VerticalPermutationCipher {
+    public static void main(String[] args) {
+        String inputFile = "src/main/resources/input.txt"; // Имя файла с вводной строкой
+        String encryptedFile = "src/main/resources/encrypted.txt"; // Файл для зашифрованной строки
+        String decryptedFile = "src/main/resources/decrypted.txt"; // Файл для расшифрованной строки
+
+        int key = 4;
+
+        ArrayList<Integer> permutation = new ArrayList<Integer>();// Порядок перестановки столбцов
+        permutation.addAll(Permutation(key));
+
+        try {
+            String inputString = readFromFile(inputFile);
+
+            // Шифрование
+            String encryptedString = encrypt(inputString, permutation);
+            writeToFile(encryptedFile, encryptedString);
+            System.out.println("Зашифрованная строка сохранена в файл " + encryptedFile);
+
+            // Расшифровка
+            String decryptedString = decrypt(encryptedString, permutation);
+            writeToFile(decryptedFile, decryptedString);
+            System.out.println("Расшифрованная строка сохранена в файл " + decryptedFile);
+        } catch (IOException e) {
+            System.err.println("Ошибка ввода/вывода");
+            e.printStackTrace();
+        }
+    }
+    // Метод для генерации порядка столбцов
+    public static ArrayList<Integer> Permutation(int maxNumber){
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        ArrayList<Integer> allNumbers = new ArrayList<>();
+
+        for (int i = 1; i <= maxNumber; i++) {
+            allNumbers.add(i);
+        }
+
+        Random random = new Random();
+        while (!allNumbers.isEmpty()) {
+            int randomIndex = random.nextInt(allNumbers.size());
+            int randomNumber = allNumbers.remove(randomIndex);
+            randomNumbers.add(randomNumber);
+        }
+        return randomNumbers;
+    }
+    // Метод для шифрования строки по вертикальной перестановке
+    public static String encrypt(String input, ArrayList<Integer> permutation) {
+        System.out.println( permutation.size());
+        char[][] grid = new char[permutation.size()][input.length() / permutation.size()];
+        StringBuilder encrypted = new StringBuilder();
+
+        int index = 0;
+        for (int i = 0; i < permutation.size(); i++) {
+            for (int j = 0; j < input.length() / permutation.size(); j++) {
+                grid[permutation.get(i) - 1][j] = input.charAt(index++);
+            }
+        }
+
+        for (int i = 0; i < input.length() / permutation.size(); i++) {
+            for (int j = 0; j < permutation.size(); j++) {
+                encrypted.append(grid[j][i]);
+            }
+        }
+
+        return encrypted.toString();
+    }
+
+    // Метод для расшифровки строки по вертикальной перестановке
+    public static String decrypt(String input, ArrayList<Integer> permutation) {
+        char[][] grid = new char[permutation.size()][input.length() / permutation.size()];
+        StringBuilder decrypted = new StringBuilder();
+
+        int index = 0;
+        for (int i = 0; i < input.length() / permutation.size(); i++) {
+            for (int j = 0; j < permutation.size(); j++) {
+                grid[j][i] = input.charAt(index++);
+            }
+        }
+
+        for (int i = 0; i < permutation.size(); i++) {
+            for (int j = 0; j < input.length() / permutation.size(); j++) {
+                decrypted.append(grid[permutation.get(i) - 1][j]);
+            }
+        }
+
+        return decrypted.toString();
+    }
+    // Метод для чтения строки из файла
+    public static String readFromFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line = reader.readLine();
+        reader.close();
+        return line;
+    }
+    // Метод для записи строки в файл
+    public static void writeToFile(String filename, String content) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write(content);
+        writer.close();
+    }
+}
